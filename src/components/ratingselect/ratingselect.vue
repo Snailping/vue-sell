220f3129a5b95cfafb8301927647bf3c4e851c7a
wkl--007 <499657357@qq.com>
2018-08-22 11:03:12 +0800

    新增页面

---
+++ src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data () {
+      return {}
+    },
+    created () {
+
+    },
+    methods: {},
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+
+</style>
